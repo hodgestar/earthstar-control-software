7bc63739511610453fa9ec9e5f2a9af96c110f03
Simon Cross <hodgestar@gmail.com>
2018-03-02 23:06:34 +0200

    Pre-calculate dotes.

--- earthstar/effects/spindots.py
+++ earthstar/effects/spindots.py
@@ -12,18 +12,17 @@ class SpindotsEffect(Effect):
     EFFECT = "spindots"
 
     def post_init(self, **kw):
-        self.ring = kw.get("ring")
-        self.pos = int(np.round(
-            self.fc.leds_per_ring * (1. + (kw["angle"] / np.pi))))
-        self.pos = np.clip(self.pos, 5, self.fc.leds_per_ring - 1)
         self.ticks = 100
+        self.dots = np.zeros(self.fc.ring_shape, dtype=self.fc.frame_dtype)
+        for i in range(5):
+            self.dots[i] = [255, 215, 0]
+        pos = int(np.round(
+            self.fc.leds_per_ring * (1. + (kw["angle"] / np.pi))))
+        self.dots = np.roll(self.dots, pos, axis=0)
 
     def apply(self, frame):
-        for i in range(5):
-            frame[self.ring, self.pos - i, :] = [255, 215, 0]
-        self.pos += 1
-        if self.pos >= self.fc.leds_per_ring:
-            self.pos = 5
-        # frame[self.ring] = np.roll(frame[self.ring], 1, axis=1)
+        self.dots = np.roll(self.dots, 1, axis=0)
+        frame[self.ring] = np.where(
+            self.dots != 0, self.dots, frame[self.ring])
         self.ticks -= 1
         return self.ticks > 0
