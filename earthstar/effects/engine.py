5c1f8bb40ac6ff51f8e4bd682b0d52fb850219a4
Simon Cross <hodgestar@gmail.com>
2018-03-02 23:41:43 +0200

    Add effect argument support.

--- earthstar/effects/engine.py
+++ earthstar/effects/engine.py
@@ -2,6 +2,8 @@
 
 """ Engine and base classes for applying effects. """
 
+import random
+
 import numpy as np
 
 from .. import frame_utils
@@ -47,11 +49,18 @@ class Effect:
     """ Base effect class. """
 
     EFFECT = "unknown"
+    ARGS = {}
 
     def __init__(self, frame_constants, ring, **kw):
         self.fc = frame_constants
         self.ring = ring
-        self.post_init(**kw)
+        self.post_init(**self._extract_args(kw))
+
+    def _extract_args(self, kw):
+        return {
+            name: argtype(kw.get(name, None))
+            for name, argtype in self.ARGS.items()
+        }
 
     def post_init(self, **kw):
         pass
@@ -64,3 +73,59 @@ class Effect:
                 the effect.
         """
         return False
+
+
+class IntArg:
+    def __init__(self, default, min=None, max=None):
+        self._default = default
+        self._min = min
+        self._max = max
+
+    def __call__(self, v):
+        try:
+            v = int(v)
+        except Exception:
+            return self._default
+        if self._min is not None:
+            v = max(v, self._min)
+        if self._max is not None:
+            v = min(v, self._max)
+        return v
+
+
+class FloatArg:
+    def __init__(self, default, min=None, max=None):
+        self._default = default
+        self._min = min
+        self._max = max
+
+    def __call__(self, v):
+        try:
+            v = float(v)
+        except Exception:
+            return self._default
+        if self._min is not None:
+            v = max(v, self._min)
+        if self._max is not None:
+            v = min(v, self._max)
+        return v
+
+
+class ColourArg:
+
+    DEFAULT_COLOURS = [
+        (255, 0, 0),
+        (0, 255, 0),
+        (0, 0, 255),
+        (255, 215, 0),
+    ]
+
+    def __init__(self):
+        pass
+
+    def __call__(self, v):
+        try:
+            v = (int(v[0]), int(v[1]), int(v[2]))
+        except Exception:
+            v = random.choice(self.DEFAULT_COLOURS)
+        return np.array(v)
