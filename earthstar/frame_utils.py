4a4b7e59bbe36ae9eef943e7cb33dab9cf16069e
Simon Cross <hodgestar@gmail.com>
2018-04-01 20:41:11 +0200

    Add ring size fractions and crossings.

--- earthstar/frame_utils.py
+++ earthstar/frame_utils.py
@@ -7,11 +7,19 @@
 
 import numpy as np
 
+# Fundamental constants of the ring universe
 N_RINGS = 6
 LEDS_PER_RING = 450
 FRAME_SHAPE = (N_RINGS, LEDS_PER_RING, 3)
 RING_SHAPE = (LEDS_PER_RING, 3)
 FRAME_DTYPE = np.uint8
+
+# Derived numbers of LEDs for a given fraction of a circle
+C = LEDS_PER_RING
+C2 = LEDS_PER_RING / 2
+C4 = LEDS_PER_RING / 4
+C8 = FTS = LEDS_PER_RING / 8  # full triangle side
+C16 = HTS = LEDS_PER_RING / 16  # half triange side
 
 
 class FrameConstants:
@@ -21,6 +29,19 @@ class FrameConstants:
     frame_shape = FRAME_SHAPE
     ring_shape = RING_SHAPE
     frame_dtype = FRAME_DTYPE
+    c = C
+    c2 = C2
+    c4 = C4
+    c8 = fts = C8
+    c16 = hts = C16
+    crossings = {
+        (3, HTS): (4, -HTS % C),  # ground triangle
+        (4, HTS): (5, -HTS % C),
+        (5, HTS): (3, -HTS % C),
+        (3, C2 + HTS): (4, C2 - HTS),  # sky triangle
+        (4, C2 + HTS): (5, C2 - HTS),
+        (5, C2 + HTS): (3, C2 - HTS),
+    }
 
 
 def candy_stripes():
