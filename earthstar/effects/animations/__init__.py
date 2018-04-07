1cb97a0979f97a2079ed30714bb9187f4461424b
Simon Cross <hodgestar@gmail.com>
2018-04-07 16:59:22 +0200

    Add animation importer.

--- earthstar/effects/animations/__init__.py
+++ earthstar/effects/animations/__init__.py
@@ -2,6 +2,7 @@
 
 """ Animations package. """
 
+from ..engine import Animation
 from .ground_and_sky import GroundAndSky
 from .lots_of_dots import LotsOfDots
 
@@ -9,3 +10,15 @@ DEFAULT_ANIMATIONS = [
     GroundAndSky,
     LotsOfDots,
 ]
+
+
+def import_animation(name):
+    """ Import an animation class by module name. """
+    fullname = "{}.{}".format(__name__, name)
+    module = __import__(fullname)
+    for part in fullname.split(".")[1:]:
+        module = getattr(module, part)
+    objs = [getattr(module, x) for x in dir(module) if not x.startswith("_")]
+    objs = [x for x in objs if type(x) == type and issubclass(x, Animation)]
+    objs = [x for x in objs if x is not Animation]
+    return objs[0]
