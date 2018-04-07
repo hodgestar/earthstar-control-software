4c568731cc2f10a0cb33bc86a4bdbc74438c1432
Simon Cross <hodgestar@gmail.com>
2018-04-08 00:30:38 +0200

    Re-implement worms in terms of segments.

--- earthstar/effects/units/worm.py
+++ earthstar/effects/units/worm.py
@@ -7,61 +7,58 @@ from ..engine import Unit
 
 
 class Worm(Unit):
-    def __init__(self, fc, ring, pos, speed, length, colour):
+    """ A worm on a given ring that starts at the given position.
+
+        A worm has a length and a speed. Speed should always be positive.
+        Use a negative length to have a worm head in the opposite direction.
+    """
+    def __init__(self, fc, ring, start, length, speed, colour):
         self.fc = fc
-        self.ring = ring
-        self.pos = pos
         self.speed = speed
-        self.length = length
         self.colour = colour
-        self.crossings = []
+        self.segments = []  # list of (ring, start, end)
+        pos = start
+        while length != 0:
+            end = pos - length
+            end = min(max(end, 0), self.fc.c - 1)
+            self.segments.append((ring, pos, end))
+            length -= (pos - end)
+            # if 0 < end < self.fc.c, then neither max nor min
+            # was triggered and length is 0.
+            if end == 0:
+                pos = self.fc.c - 1
+            elif end == self.fc.c - 1:
+                pos = 0
 
     def step(self):
-        next_pos = self.pos
-        next_ring = self.ring
         for _ in range(self.speed):
-            # TODO: support going in the -1 direction
-            next_pos += 1
-            if next_pos >= self.fc.c:
-                self.crossings.insert(0, (next_ring, 0, next_ring, self.fc.c))
-                next_pos = 0
-            elif (next_ring, next_pos) in self.fc.crossings:
-                cross_ring, cross_pos = self.fc.crossings[
-                    (next_ring, next_pos)]
-                self.crossings.insert(0, (
-                    cross_ring, cross_pos, next_ring, next_pos))
-                next_ring, next_pos = cross_ring, cross_pos
-        self.pos = next_pos
-        self.ring = next_ring
+            f_ring, f_start, f_end = self.segments[0]
+            f_direction = (-1, 1)[f_start >= f_end]
+            n_start = f_start + f_direction
+            if n_start >= self.fc.c:
+                self.segments.insert(0, (f_ring, 1, 0))
+                self.segments[1] = (f_ring, self.fc.c - 1, f_end)
+            elif n_start <= -1:
+                self.segments.insert(0, (f_ring, self.fc.c - 2, self.fc.c - 1))
+                self.segments[1] = (f_ring, 0, f_end)
+            else:
+                self.segments[0] = (f_ring, n_start, f_end)
+                if (f_ring, n_start) in self.fc.crossings:
+                    c_ring, c_start = self.fc.crossings[(f_ring, n_start)]
+                    self.segments.insert(0, (c_ring, c_start, c_start))
+
+            l_ring, l_start, l_end = self.segments[-1]
+            l_direction = (-1, 1)[l_start >= l_end]
+            n_end = l_end + l_direction
+            if n_end == l_start:
+                del self.segments[-1]
+            else:
+                self.segments[-1] = (l_ring, l_start, n_end)
 
     def render(self, frame):
-        length_remaining = self.length
-        ring = self.ring
-        pos = self.pos
-        crossing = 0
-        print("----")
-        while (length_remaining > 0) and (crossing < len(self.crossings)):
-            cross_ring, cross_pos, next_ring, next_pos = self.crossings[
-                crossing]
-            if cross_pos < pos:
-                start, end = cross_pos, pos
-            else:
-                start, end = pos, cross_pos
+        for ring, start, end in self.segments:
+            if end < start:
+                start, end = end, start
             n = end - start
-            n = min(n, length_remaining)
-            end = start + n
-            print(("C", n, start, end, length_remaining))
             if n > 0:
                 frame[ring][start:end] = [self.colour] * n
-            ring, pos = next_ring, next_pos
-            length_remaining -= n
-        if length_remaining <= 0:
-            del self.crossings[crossing + 1:]
-        while length_remaining > 0:
-            end = min(pos + length_remaining, self.fc.c)
-            n = end - pos
-            print(("R", pos, end, n, length_remaining))
-            if n > 0:
-                frame[ring][pos:end] = [self.colour] * n
-            pos = 0
-            length_remaining -= n
