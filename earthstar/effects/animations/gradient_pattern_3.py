d8e837b92ac94de22f5f53b1bb091a03d85d9155
David Sharpe <decoydavid@gmail.com>
2018-04-17 14:06:52 +0200

    Add more colours

--- earthstar/effects/animations/gradient_pattern_3.py
+++ earthstar/effects/animations/gradient_pattern_3.py
@@ -30,9 +30,19 @@ class GradientPattern(Animation):
             self.ring_render(self.fc.colour(1, 0, 1)),
         ], dtype=np.uint8)
         self.step = 0
-        self.width = 50.0
+        self.width = 75.0
         self.max_intensity = 255.0
         self.max_point = 40.0
+        self.direction = 1
+        self.colour_combos = [
+            [self.fc.colour(1, 0, 0), self.fc.colour(0, 1, 0)],
+            [self.fc.colour(1, 0, 0), self.fc.colour(0, 0, 1)],
+            [self.fc.colour(1, 0, 0), self.fc.colour(0, 1, 1)],
+            [self.fc.colour(0, 1, 0), self.fc.colour(0, 0, 1)],
+            [self.fc.colour(0, 1, 0), self.fc.colour(1, 0, 1)],
+            [self.fc.colour(0, 0, 1), self.fc.colour(1, 1, 0)],
+        ]
+        self.cycle_speed = 100
 
     def render(self, frame):
         return self.animation(frame)
@@ -44,13 +54,18 @@ class GradientPattern(Animation):
         """
         for ring in range(self.fc.n_rings):
             intensity = [0.0] * self.fc.leds_per_ring
+            self.max_point += self.direction
+            if self.direction > 0 and self.max_point >= (self.width - 1):
+                self.direction = -1
+            elif self.direction < 0 and self.max_point <= 1:
+                self.direction = 1
             gradient_up = self.max_intensity / self.max_point
             gradient_down = -1 * (self.max_intensity / (self.width - self.max_point))
             for i in range(self.fc.leds_per_ring):
                 gradient = gradient_up if (i % self.width) < self.max_point else gradient_down
                 intensity[i] = intensity[i - 1] + gradient
-            self._rings[ring] = [i * self.fc.colour(1, 0, 0) for i in intensity]
-            # self._rings[i] = np.roll(self._rings[i], 4, axis=0)
-        print(gradient_up, gradient_down)
-        print([int(i) for i in intensity])
+            self._rings[ring] = [i * self.colour_combos[ring][0] for i in intensity]
+            self._rings[ring] = np.roll(self._rings[ring], int(self.width / 3), axis=0)
+            self._rings[ring] = np.add(self._rings[ring], [i * self.colour_combos[ring][1] for i in intensity])
+
         frame[:] = self._rings
