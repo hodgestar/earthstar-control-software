026751c70fd0ec2dd4d8f9cabe31cdeb1338fecf
Simon Cross <hodgestar@gmail.com>
2018-04-19 21:59:01 +0200

    Start of tests.

---
+++ tests/effects/animations/test_generic.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+""" Generic tests for all animations.
+
+    These tests run against all animation classes found in
+    earthstar.effects.animations.*.
+"""
+
+import pytest
+
+
+def test_generates_one_hundred_frames():
+    """ Tests that each animation can generate one hundred
+        frames correctly in a reasonable amount of time.
+    """
+    pass
