c78f115fbd3ef9061771cbc77c70f69e72c776bc
Simon Cross <hodgestar@gmail.com>
2018-04-19 22:18:06 +0200

    Add tests that animations can render 100 frames.

--- tests/effects/animations/test_generic.py
+++ tests/effects/animations/test_generic.py
@@ -6,12 +6,28 @@
     earthstar.effects.animations.*.
 """
 
+import glob
+import os
+
 import pytest
 
-ANIMATIONS = [
-    "a",
-    "b",
-]
+import earthstar.effects.animations as animations
+from earthstar.effects.engine import EffectEngine
+from earthstar.frame_utils import FrameConstants
+
+
+def find_animations():
+    pkg_folder = os.path.dirname(animations.__file__)
+    pkg_modules = [
+        os.path.splitext(os.path.basename(x))[0]
+        for x in glob.glob(pkg_folder + "/*.py")
+    ]
+    return [
+        animations.import_animation(x) for x in pkg_modules
+    ]
+
+
+ANIMATIONS = find_animations()
 
 
 @pytest.mark.parametrize("animation_cls", ANIMATIONS)
@@ -19,4 +35,10 @@ def test_generates_one_hundred_frames(animation_cls):
     """ Tests that each animation can generate one hundred
         frames correctly in a reasonable amount of time.
     """
-    pass
+    fc = FrameConstants()
+    engine = EffectEngine(fc=fc, tick=1. / 10, transition=60)
+    engine.add_animation_type(animation_cls)
+    for i in range(100):
+        frame = engine.next_frame()
+        assert frame.shape == fc.frame_shape
+        assert frame.dtype == fc.frame_dtype
